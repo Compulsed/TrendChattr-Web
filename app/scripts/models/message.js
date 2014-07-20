6f3a558545dc0b3b7ca73948ce236c59ab6d9571
Dale Salter <dale@192-168-1-126.tpgi.com.au>
2014-07-20 19:38:21 +1000

    Working state before seperation of messages

--- app/scripts/models/message.js
+++ app/scripts/models/message.js
@@ -1,63 +1,37 @@
-// Trendchattr.Message = DS.Model.extend({
-// 	username: DS.attr('string'),
-// 	message: DS.attr('string')
-// });
+Trendchattr.Message = DS.Model.extend({
+	username: DS.attr('string'),
+	message: DS.attr('string')
+});
 
-// Trendchattr.Message.FIXTURES = [{
-//     id: 1,
-//     username: "dale",
-//     message: "I'm so swag",
-//     room: "swag"
-// }, {
-//     id: 2,
-//     username: "dan",
-//     message: "I know you are",
-//     room: "swag"
-// }];
+Trendchattr.Message.FIXTURES = [{
+    id: 1,
+    username: "dale",
+    message: "swag",
+    room: "swag"
+}, {
+    id: 2,
+    username: "dan",
+    message: "I know you are",
+    room: "swag"
+}];
 
 
-
-// Trendchattr.Chatroom = DS.Model.extend({
-//     chatroom: DS.attr('string'),
-//     joined: DS.attr('string')
-// });
-
-// Trendchattr.Chatroom.FIXTURES = [{
-//     id: 1,
-//     chatroom: "Room Dale",
-//     joined: "10"
-// }, {
-//     id: 2,
-//     chatroom: "Room Dan",
-//     joined: "50"
-// }];
 
 Trendchattr.Chatroom = DS.Model.extend({
     chatroom: DS.attr('string'),
     joined: DS.attr('string'),
-    sent: DS.attr()
+    comments: DS.attr([])
 });
 
 Trendchattr.Chatroom.FIXTURES = [{
     id: 1,
     chatroom: "Room Dale",
     joined: "10",
-    sent: [{
-        username: "dan",
-        message: "dale is so cool"
-    },{
-        username: "dale",
-        message: "yeah, I know"
-    }]
+    comments: ["Swag", "Lol", "XD"]
 }, {
     id: 2,
     chatroom: "Room Dan",
-    joined: "20",
-    sent: [{
-        username: "dan",
-        message: "dale is so cool"
-    },{
-        username: "dale",
-        message: "yeah, I know"
-    }]
+    joined: "50",
+    comments: ["This is cool", "YEAH I KNOW"]
 }];
+
